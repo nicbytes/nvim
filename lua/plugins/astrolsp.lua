285ddc0a433c7a45523e9814b1d963c13b7eb970
Nicholas Buckeridge <bucknich@gmail.com>
2025-09-16 10:46:21 +1000

    fix(formatter): typescript stop double formate with prettier & eslint

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -3,6 +3,21 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
+    autocmds = {
+      -- Prevent double formatting
+      -- Ref: https://discord.com/channels/939594913560031363/1224731564261507072/1367373738617868289
+      -- Ref: https://github.com/AstroNvim/astrocommunity/blob/ced2b71ebe41d43e2268129caffcf96433e43f8f/lua/astrocommunity/pack/typescript/init.lua#L92-L101
+      eslint_fix_on_save = {
+        cond = function(client) return client.name == "eslint" and vim.fn.exists ":EslintFixAll" > 0 end,
+        {
+          event = "BufWritePost",
+          desc = "Fix all eslint errors",
+          callback = function(args)
+            if vim.F.if_nil(vim.b[args.buf].autoformat, vim.g.autoformat, true) then vim.cmd.EslintFixAll() end
+          end,
+        },
+      },
+    },
     servers = {
       "rust_analyzer",
     },
